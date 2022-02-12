1587707403d6433b73ef2a07477bf7c7ccc14f83
Abdullah Alnuaimi <abdullahalnuaimi36@gmail.com>
2022-02-12 23:10:04 +0300

    fixed user manager issues

--- realworld/apps/accounts/admin.py
+++ realworld/apps/accounts/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("name", "email", "is_staff", "is_active", "date_joined")
+    list_display_links = ("email",)
+    search_fields = ("name", "bio", "email")
+    ordering = ("name",)
+
+    fieldsets = (
+        (None, {"fields": ("name", "password")}),
+        ("Personal info", {"fields": ("email", "bio", "image")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("name", "password1", "password2"),
+            },
+        ),
+    )
--- realworld/apps/accounts/models.py
+++ realworld/apps/accounts/models.py
@@ -5,18 +5,37 @@ from django.db import models
 from django.urls import reverse
 
 
-class UserManager(BaseUserManager):
-    def create_user(self, email: str, password: str | None = None, **other_fields) -> User:
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
 
-        user = User(email=email, **other_fields)
-
-        if password:
-            user.set_password(password)
-        else:
-            user.set_unusable_password()
-
+    def create_user(self, email: str, password: str, **extra_fields) -> User:
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError("The Email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
         user.save(using=self.db)
         return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -26,18 +45,20 @@ class User(AbstractUser):
     first_name = None
     last_name = None
 
-    email: str = models.EmailField("Email Address", unique=True)
+    email: str = models.EmailField("Email address", unique=True)
     name: str = models.CharField(max_length=60)
     bio: str = models.TextField(blank=True)
     image: str | None = models.URLField(null=True, blank=True)
 
     followers = models.ManyToManyField("self", blank=True)
 
-    EMAIL_FIELD = "email"
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
-    objects = UserManager()
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.name
 
     def get_absolute_url(self) -> str:
         return reverse("profile", args=[self.id])
